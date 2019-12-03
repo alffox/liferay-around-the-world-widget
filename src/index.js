a173f14bad31a05b2b95a1094e06914ba08c3a57
alffox <alfonso.crisci.it@gmail.com>
2019-12-03 10:12:59 +0100

    Feat: Add header

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import AppComponent from './AppComponent';
-
+//import AppComponent from "./AppComponent";
+import AtwHeader from "./modules/AtwHeader.es";
 
 /**
  * This is the main entry point of the portlet.
  *
- * See https://tinyurl.com/js-ext-portlet-entry-point for the most recent 
+ * See https://tinyurl.com/js-ext-portlet-entry-point for the most recent
  * information on the signature of this function.
  *
  * @param  {Object} params a hash with values of interest to the portlet
  * @return {void}
  */
-export default function main({portletNamespace, contextPath, portletElementId, configuration}) {
-    
-    ReactDOM.render(
-        <AppComponent 
-            portletNamespace={portletNamespace} 
-            contextPath={contextPath}
-            portletElementId={portletElementId}
-            
-            configuration={configuration}
-            
-            />, 
-        document.getElementById(portletElementId)
-    );
-    
+export default function main({
+  //   portletNamespace,
+  //   contextPath,
+  portletElementId
+  //   configuration
+}) {
+  ReactDOM.render(
+    <div className="container-fluid">
+      <AtwHeader />
+      {/* <AppComponent
+        portletNamespace={portletNamespace}
+        contextPath={contextPath}
+        portletElementId={portletElementId}
+        configuration={configuration}
+      /> */}
+    </div>,
+    document.getElementById(portletElementId)
+  );
 }
---
+++ src/modules/AtwHeader.es.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class AtwHeader extends React.Component {
+  render() {
+    return (
+      <header className="row">
+        <div className="col-md-12 text-center text-uppercase">
+          <img
+            className="float-left img-fluid favicon"
+            src="/o/liferay-around-the-world/images/logo.svg"
+            alt="Paper airplane icon"
+          />
+          <h1 className="title-super position-relative d-none d-sm-block">
+            Liferay Around the World
+          </h1>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default AtwHeader;
