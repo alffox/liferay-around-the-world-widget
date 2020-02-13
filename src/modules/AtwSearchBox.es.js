dca0889c357a1bbdc66efd68310a890069d835fc
alffox <alfonso.crisci.it@gmail.com>
2020-02-13 15:17:20 +0100

    bug: Prevent form submission

--- src/modules/AtwSearchBox.es.js
+++ src/modules/AtwSearchBox.es.js
@@ -1,9 +1,22 @@
 import React from "react";
 
 class AtwSearchBox extends React.Component {
+
+    filterList(event) { // Feature inspired by https://www.jondjones.com/frontend/react/components/how-to-build-a-filterable-search-bar-in-react
+        let currentFlags = this.state.initialFlags;
+        currentFlags = currentFlags.filter((location) => {
+            return location.title.toLowerCase().search(event.target.value.toLowerCase()) !== -1;
+        });
+        this.setState({ currentFlags: currentFlags });
+    }
+
+    preventSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form className="form-inline my-2 my-lg-0">
+            <form className="form-inline my-2 my-lg-0" onSubmit={this.preventSubmit}>
                 <input className="form-control mr-sm-2" type="search" placeholder="Filter Locations ..." aria-label="Search" onChange={this.props.filterList} />
             </form>
         );
