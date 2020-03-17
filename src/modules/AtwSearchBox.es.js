71e50d9f0d53028e76b8087948c00e9bf309e6b0
alffox <alfonso.crisci.it@gmail.com>
2020-03-17 10:29:29 +0100

    bug: Remove testing CSS class

--- src/modules/AtwSearchBox.es.js
+++ src/modules/AtwSearchBox.es.js
@@ -16,7 +16,7 @@ class AtwSearchBox extends React.Component {
 
     render() {
         return (
-            <form className="form-inline my-2 my-lg-0" onSubmit={this.preventSubmit}>
+            <form className="form-inline" onSubmit={this.preventSubmit}>
                 <input className="form-control mr-sm-2" type="search" placeholder="Find Location ..." aria-label="Search" onChange={this.props.filterList} />
             </form>
         );
